7bf63022c6082f66c3fd9c663813182356be3017
Sam Browne <sammobrowne@gmail.com>
2023-09-01 23:10:08 +0800

    login

--- src/modules/core/login/login.component.ts
+++ src/modules/core/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  form = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
 onSubmit() {
-throw new Error('Method not implemented.');
 }
 
 }
