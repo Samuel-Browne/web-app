d0b40fc3480d366123d74cb1f8a2ce842839e358
Sam <sammobrowne@gmail.com>
2023-09-18 19:50:48 +0800

    subsink pokemon service

--- src/modules/core/home/home.component.ts
+++ src/modules/core/home/home.component.ts
@@ -1,81 +1,88 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { SubSink } from 'subsink';
+import { PokemonService } from 'src/modules/shared/services/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  private subs = new SubSink();
 
-pictureUrl: string = "";
+  pictureUrl: string = '';
 
-firstGenSpriteUrls: string[] = [];
+  firstGenSpriteUrls: string[] = [];
 
-secondGenSpriteUrls: string[] = [];
+  secondGenSpriteUrls: string[] = [];
 
-thirdGenSpriteUrls: string[] = [];
-fourthGenSpriteUrls: string[] = [];
-fifthGenSpriteUrls: string[] = [];
-sixthGenSpriteUrls: string[] = [];
-seventhGenSpriteUrls: string[] = [];
+  thirdGenSpriteUrls: string[] = [];
+  fourthGenSpriteUrls: string[] = [];
+  fifthGenSpriteUrls: string[] = [];
+  sixthGenSpriteUrls: string[] = [];
+  seventhGenSpriteUrls: string[] = [];
 
-constructor(private httpClient: HttpClient){
+  constructor(private pokemonService: PokemonService) {
+    for (var i = 0; i < 10; i++) {
+      this.subs.sink = this.pokemonService
+        .getPokemonData(i)
+        .subscribe((data) => {
+          this.firstGenSpriteUrls.push(data.sprites.front_default);
+        });
+    }
 
-  for(var i = 0; i < 10; i++){
-    this.getImage("https://pokeapi.co/api/v2/pokemon/" + i).subscribe(data =>{
-      this.firstGenSpriteUrls.push(data.sprites.front_default);
-    });
+    for (var i = 0; i < 9; i++) {
+      this.subs.sink = this.pokemonService
+        .getPokemonData(152 + i)
+        .subscribe((data) => {
+          this.secondGenSpriteUrls.push(data.sprites.front_default);
+        });
+    }
+
+    for (var i = 0; i < 9; i++) {
+      this.subs.sink = this.pokemonService
+        .getPokemonData(252 + i)
+        .subscribe((data) => {
+          this.thirdGenSpriteUrls.push(data.sprites.front_default);
+        });
+    }
+
+    for (var i = 0; i < 9; i++) {
+      this.subs.sink = this.pokemonService
+        .getPokemonData(387 + i)
+        .subscribe((data) => {
+          this.fourthGenSpriteUrls.push(data.sprites.front_default);
+        });
+    }
+
+    for (var i = 0; i < 9; i++) {
+      this.subs.sink = this.pokemonService
+        .getPokemonData(495 + i)
+        .subscribe((data) => {
+          this.fifthGenSpriteUrls.push(data.sprites.front_default);
+        });
+    }
+
+    for (var i = 0; i < 9; i++) {
+      this.subs.sink = this.pokemonService
+        .getPokemonData(650 + i)
+        .subscribe((data) => {
+          this.sixthGenSpriteUrls.push(data.sprites.front_default);
+        });
+    }
+
+    for (var i = 0; i < 9; i++) {
+      this.subs.sink = this.pokemonService
+        .getPokemonData(722 + i)
+        .subscribe((data) => {
+          this.seventhGenSpriteUrls.push(data.sprites.front_default);
+        });
+    }
   }
 
-  for(var i = 0; i < 9; i++){
-    this.getImage("https://pokeapi.co/api/v2/pokemon/" + (152 + i)).subscribe(data =>{
-      this.secondGenSpriteUrls.push(data.sprites.front_default);
-    });
+  onClick() {}
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
-
-  for(var i = 0; i < 9; i++){
-    this.getImage("https://pokeapi.co/api/v2/pokemon/" + (252 + i)).subscribe(data =>{
-      this.thirdGenSpriteUrls.push(data.sprites.front_default);
-    });
-  }
-
-  for(var i = 0; i < 9; i++){
-    this.getImage("https://pokeapi.co/api/v2/pokemon/" + (387 + i)).subscribe(data =>{
-      this.fourthGenSpriteUrls.push(data.sprites.front_default);
-    });
-  }
-
-  for(var i = 0; i < 9; i++){
-    this.getImage("https://pokeapi.co/api/v2/pokemon/" + (495 + i)).subscribe(data =>{
-      this.fifthGenSpriteUrls.push(data.sprites.front_default);
-    });
-  }
-
-  for(var i = 0; i < 9; i++){
-    this.getImage("https://pokeapi.co/api/v2/pokemon/" + (650 + i)).subscribe(data =>{
-      this.sixthGenSpriteUrls.push(data.sprites.front_default);
-    });
-  }
-
-  for(var i = 0; i < 9; i++){
-    this.getImage("https://pokeapi.co/api/v2/pokemon/" + (722 + i)).subscribe(data =>{
-      this.seventhGenSpriteUrls.push(data.sprites.front_default);
-    });
-  }
-
-
-
-
-
-}
-getImage(imageUrl: string): Observable<any> {
-  return this.httpClient.get(imageUrl);
-}
-
-onClick(){
-  
-}
-
 }
---
+++ src/modules/shared/services/pokemon.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getPokemonData(pokedexNumber: number): Observable<any>{
+    return this.httpClient.get("https://pokeapi.co/api/v2/pokemon/" + pokedexNumber);
+  }
+    
+}
