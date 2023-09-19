1f9160795ed5c90c699aea923dff1c418fe516c1
Sam <sammobrowne@gmail.com>
2023-09-20 00:50:36 +0800

    about

--- src/modules/core/about/about.component.ts
+++ src/modules/core/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PokemonService } from 'src/modules/shared/services/pokemon.service';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
-  ngOnInit(){
-    
+  pokemon: any;
+  id: number;
+
+  constructor(private router: ActivatedRoute ,private pokemonService: PokemonService){
+    this.id = this.router.snapshot.params['id'];
+    this.pokemonService.getPokemonData(this.id).subscribe(data => {
+      this.pokemon = data;
+      console.log(this.pokemon);
+    });
+  
   }
+  ngOnInit(){}
+
 }
