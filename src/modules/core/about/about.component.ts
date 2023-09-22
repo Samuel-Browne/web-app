803df18268004e5735e4d775a7bc86f166eec20f
Sam <sammobrowne@gmail.com>
2023-09-23 01:46:16 +0800

    navigation....

--- src/modules/core/about/about.component.ts
+++ src/modules/core/about/about.component.ts
@@ -5,41 +5,50 @@ import { PokemonService } from 'src/modules/shared/services/pokemon.service';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
-
   pokemon: any;
   species: any;
   id: number;
 
-  constructor(private activatedRoute: ActivatedRoute , private router: Router, private pokemonService: PokemonService){
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private pokemonService: PokemonService
+  ) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => {
+      return false;
+    };
+
     this.id = this.activatedRoute.snapshot.params['id'];
-    this.pokemonService.getPokemonData(this.id).subscribe(data => {
+    this.pokemonService.getPokemonData(this.id).subscribe((data) => {
       this.pokemon = data;
       console.log(this.pokemon);
     });
 
-    this.pokemonService.getPokemonSpeciesData(this.id).subscribe(data => {
+    this.pokemonService.getPokemonSpeciesData(this.id).subscribe((data) => {
       this.species = data;
       console.log(this.species);
       let ok: string = this.species.flavor_text_entries[0].flavor_text;
 
       this.species.flavor_text_entries[0].flavor_text = ok.replace(/\f/g, ' ');
-        
     });
-
-
-
-  
   }
-  navigateForwards(){
-      this.router.navigate(["" + (this.id++)]);
+  navigateForwards(event: Event) {
+    if (this.id == 1017) {
+      return;
+    }
+    this.id++;
+    this.router.navigate(['' + this.id]);
   }
 
-  navigateBackwards(){
-    this.router.navigate(["" + (this.id--)]);
+  navigateBackwards(event: Event) {
+    if (this.id == 1) {
+      return;
+    }
+    this.id--;
+    this.router.navigate(['' + this.id]);
   }
-  ngOnInit(){}
-
+  ngOnInit() {}
 }
